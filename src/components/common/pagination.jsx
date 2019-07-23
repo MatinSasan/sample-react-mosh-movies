5f25d81f691c1a8455d7075eaf3e80bc510556ec
Matin Sasanpour <matin.sasan@gmail.com>
2019-07-23 16:08:31 +0430

    added protype to pagination in common folder | replaced a tag with button to follow the rules in pagination

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import _ from 'lodash';
 
 const Pagination = props => {
@@ -15,14 +16,21 @@ const Pagination = props => {
           <li
             className={page === currentPage ? 'page-item active' : 'page-item'}
             key={page}>
-            <a className="page-link" onClick={() => onPageChange(page)}>
+            <button className="page-link" onClick={() => onPageChange(page)}>
               {page}
-            </a>
+            </button>
           </li>
         ))}
       </ul>
     </nav>
   );
+};
+
+Pagination.propTypes = {
+  itemsCount: PropTypes.number.isRequired,
+  pageSize: PropTypes.number.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired
 };
 
 export default Pagination;
