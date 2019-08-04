117666788c0cb22477c208041973e12cc11badaf
Matin Sasanpour <matin.sasan@gmail.com>
2019-08-04 07:39:30 +0430

    excluded sorting function into its own component

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -46,14 +46,7 @@ export class Movies extends Component {
     this.setState({ selectedGenre: genre, currentPage: 1 });
   };
 
-  handleSort = path => {
-    const sortColumn = { ...this.state.sortColumn };
-    if (sortColumn.path === path) {
-      sortColumn.order = sortColumn.order === 'asc' ? 'desc' : 'asc';
-    } else {
-      sortColumn.path = path;
-      sortColumn.order = 'asc';
-    }
+  handleSort = sortColumn => {
     this.setState({ sortColumn });
   };
 
@@ -91,6 +84,7 @@ export class Movies extends Component {
 
           <MoviesTable
             movies={movies}
+            sortColumn={sortColumn}
             onLike={this.handleLiked}
             onDelete={this.handleDelete}
             onSort={this.handleSort}
