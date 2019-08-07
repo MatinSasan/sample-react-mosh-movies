857aaf2a58f1e7d162062d9bba31917c350ce4f3
Matin Sasanpour <matin.sasan@gmail.com>
2019-08-07 10:52:47 +0430

    added navbar component and implemented

---
+++ src/components/navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/">
+        Dreamy
+      </Link>
+
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/moviess">
+              Movies <span className="sr-only">(current)</span>
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/customers">
+              Customers
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/rentals">
+              Rentals
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
