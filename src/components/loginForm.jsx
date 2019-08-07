4a4e1e7019b3bfc364b80a37da2f85fb6573e446
Matin Sasanpour <matin.sasan@gmail.com>
2019-08-07 11:50:10 +0430

    added logic for values for loginform

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,18 +1,51 @@
 import React, { Component } from 'react';
 
 export class LoginForm extends Component {
+  state = {
+    account: { username: '', password: '' }
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    console.log('submitted', `${this.username} : ${this.password}`);
+  };
+
   render() {
+    const { username, password } = this.state.account;
+
     return (
       <div>
         <h1>Login</h1>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" className="form-control" />
+            <input
+              autoFocus
+              name="username"
+              value={username}
+              onChange={this.handleChange}
+              id="username"
+              type="text"
+              className="form-control"
+            />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
+            <input
+              name="password"
+              value={password}
+              onChange={this.handleChange}
+              id="password"
+              type="text"
+              className="form-control"
+            />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
