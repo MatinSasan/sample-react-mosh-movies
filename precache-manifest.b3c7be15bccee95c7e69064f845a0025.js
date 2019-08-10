f37443cf6fbd739d786c2a17e5f28f99b0301114
Matin Sasanpour <matin.sasan@gmail.com>
2019-08-10 16:19:47 +0430

    Updates

---
+++ precache-manifest.b3c7be15bccee95c7e69064f845a0025.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e11514af91729ea8ffd64d2dae36498",
+    "url": "/MatinSasan/sample-react-mosh-movies/index.html"
+  },
+  {
+    "revision": "da7d5f8b4e4fb1648e35",
+    "url": "/MatinSasan/sample-react-mosh-movies/static/css/2.a0a65020.chunk.css"
+  },
+  {
+    "revision": "345a7f68c109444e58d9",
+    "url": "/MatinSasan/sample-react-mosh-movies/static/css/main.d7c3e0e7.chunk.css"
+  },
+  {
+    "revision": "da7d5f8b4e4fb1648e35",
+    "url": "/MatinSasan/sample-react-mosh-movies/static/js/2.e01ad5b1.chunk.js"
+  },
+  {
+    "revision": "345a7f68c109444e58d9",
+    "url": "/MatinSasan/sample-react-mosh-movies/static/js/main.1c395473.chunk.js"
+  },
+  {
+    "revision": "51dc06e880376cd59f48",
+    "url": "/MatinSasan/sample-react-mosh-movies/static/js/runtime~main.11170fb3.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/MatinSasan/sample-react-mosh-movies/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/MatinSasan/sample-react-mosh-movies/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/MatinSasan/sample-react-mosh-movies/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/MatinSasan/sample-react-mosh-movies/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/MatinSasan/sample-react-mosh-movies/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
